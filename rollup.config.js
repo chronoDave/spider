4edde898fd9e66ce9096f1fbce5b551682437385
chronoDave <davidwolters6758@gmail.com>
2024-08-18 13:18:09 +0100

    Added build

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import esbuild from 'rollup-plugin-esbuild';
+import dts from 'rollup-plugin-dts';
+
+const input = 'src/index.ts';
+const output = type => `dist/spider.${type}`;
+
+export default [{
+  input,
+  plugins: [
+    esbuild({
+      target: 'esnext'
+    })
+  ],
+  external: [
+    'fast-glob',
+    'path',
+    'fs/promises'
+  ],
+  output: [{
+    file: output('js'),
+    exports: 'auto',
+    format: 'es'
+  }]
+}, {
+  input,
+  plugins: [dts()],
+  output: {
+    file: output('d.ts'),
+    format: 'es'
+  }
+}];
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export type { Meta, WriteOptions, File } from './lib/write/write';
+export type { Bundle, BundleOptions } from './lib/bundle/bundle';
+
+export { default as bundle } from './lib/bundle/bundle';
+export { default as write } from './lib/write/write';
