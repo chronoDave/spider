7ace17ef39b0ee780fe5617b6c2a652ad2000539
David Wolters <d.t.wolters@bradford.ac.uk>
2026-04-22 10:02:59 +0100

    Added string count & improved slugify

--- src/lib/string.spec.ts
+++ src/lib/string.spec.ts
@@ -3,10 +3,19 @@ import test from 'node:test';
 import * as string from './string.ts';
 
 test('[string.slugify]', t => {
-  t.assert.equal(string.slugify('Foo Bar'), 'foo-bar');
-  t.assert.equal(string.slugify('Foo & Bar'), 'foo-bar');
-  t.assert.equal(string.slugify(' Foo Bar Baz '), 'foo-bar-baz');
-  t.assert.equal(string.slugify('foo-bar'), 'foo-bar');
-  t.assert.equal(string.slugify('foo-----bar        baz'), 'foo-bar-baz');
-  t.assert.equal(string.slugify('FooBar'), 'foobar');
+  t.assert.equal(string.slugify('foobar'), 'foobar', 'normal');
+  t.assert.equal(string.slugify('FooBar'), 'foobar', 'capitals');
+  t.assert.equal(string.slugify('     foobar      '), 'foobar', 'trim');
+  t.assert.equal(string.slugify('foo bar'), 'foo-bar', 'spaces');
+  t.assert.equal(string.slugify('FooBar!'), 'foobar', 'characters');
+  t.assert.equal(string.slugify('Foo & Bar'), 'foo-bar', 'condense spaces');
+  t.assert.equal(string.slugify('föòóbar'), 'fooobar', 'diacritics');
+});
+
+test('[string.count]', t => {
+  t.assert.doesNotThrow(() => string.count('^$')('^$'), 'escapes regexp');
+  t.assert.equal(string.count('/')('abc'), 0, 'no match');
+  t.assert.equal(string.count('/')('/abc'), 1, 'single match');
+  t.assert.equal(string.count('/')('/abc/def/'), 3, 'multiple match');
+  t.assert.equal(string.count('^$')('^aaa$'), 0, 'group match');
 });
--- src/lib/string.ts
+++ src/lib/string.ts
@@ -1,5 +1,11 @@
-export const slugify = (x: string) => x.toLocaleLowerCase()
+export const slugify = (x: string) => x
   .trim()
-  .replaceAll(/\s+/g, '-')
-  .replaceAll(/[^a-z0-9-]/g, '')
-  .replaceAll(/-+/g, '-');
+  .replace(/\s+/g, '-')
+  .normalize('NFD') // Compress characters
+  .replace(/(\p{Diacritic})|[^A-Za-z0-9-]/gu, '') // Replace diacritics
+  .replace(/-+/g, '-') // Condense dashes
+  .toLocaleLowerCase();
+
+export const count = (needle: string) =>
+  (haystack: string): number =>
+    haystack.match(new RegExp(RegExp.escape(needle), 'g'))?.length ?? 0;
