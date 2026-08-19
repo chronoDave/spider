c4d146ac15489683f23e286e5df089f18a4facb5
chronoDave <davidwolters6758@gmail.com>
2026-08-19 14:26:33 +0100

    Updated tests

--- test/about.feed.ts
+++ test/about.feed.ts
@@ -3,7 +3,7 @@ import type { Draft } from '../src/spider.ts';
 const page: Draft = {
   title: 'About',
   url: '/about.xml',
-  template: registry => document => document.body(registry),
+  template: registry => document => document.body?.(registry) ?? '',
   body: () => 'This is a feed'
 };
 
--- test/blogs.ts
+++ test/blogs.ts
@@ -2,7 +2,7 @@ import type { Draft } from '../src/spider.ts';
 
 const page: Draft = {
   title: 'Blogs',
-  template: registry => document => document.body(registry),
+  template: registry => document => document.body?.(registry) ?? '',
   body: () => 'This is a page'
 };
 
